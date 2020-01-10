6c7736d1e03cd4f1e944fb896e31924021e89fbe
Rory Murray <rory.christian.murray@gmail.com>
2020-01-10 15:03:02 -0500

    Import dependencies

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as import pd 
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+start = dt.datetime(2000,1,1)
+end = dt.datetime(2016,12,31)
+
